08c4c427de4db4e2c3367f69582b1539acf6fbdc
Crack-Man <65107147+Crack-Man@users.noreply.github.com>
2022-02-21 16:34:14 +1000

    Добавлены недостающие файлы

---
+++ src/views/Authorization.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="">
+        <form-auth></form-auth>
+    </div>
+</template>
+
+<script>
+import FormAuthorization from '@/components/FormAuthorization.vue'
+
+export default {
+    name: "Authorization",
+    components: {
+        'form-auth': FormAuthorization,
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
